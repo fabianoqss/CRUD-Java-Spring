72c1a906b90eaa5b907eaaed47ba6c4b81ad5210
Fabiano <fabianoquirinos@outlook.com>
2025-07-01 14:28:18 -0300

    :sparkles: feat: Criação da minha classe DTO e configuração dos meus atributos

--- CRUD/CRUD/src/main/java/com/example/fabiano/project/CRUD/repository/ProductRepository.java
+++ CRUD/CRUD/src/main/java/com/example/fabiano/project/CRUD/repository/ProductRepository.java
@@ -3,5 +3,5 @@ package com.example.fabiano.project.CRUD.repository;
 import com.example.fabiano.project.CRUD.entities.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CrudRepository extends JpaRepository<Produto, Long> {
+public interface ProductRepository extends JpaRepository<Produto, Long> {
 }
