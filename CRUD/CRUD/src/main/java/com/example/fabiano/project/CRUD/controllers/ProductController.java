ad56f905b22d1e9a92529238f8210bfe3678a66c
Fabiano <fabianoquirinos@outlook.com>
2025-11-04 21:19:22 -0300

    :sparkles: feat: implementacao dos metodos Get nos Controllers

--- CRUD/CRUD/src/main/java/com/example/fabiano/project/CRUD/controllers/ProductController.java
+++ CRUD/CRUD/src/main/java/com/example/fabiano/project/CRUD/controllers/ProductController.java
@@ -1,7 +1,13 @@
 package com.example.fabiano.project.CRUD.controllers;
 
+import com.example.fabiano.project.CRUD.dto.ProdutoDTO;
 import com.example.fabiano.project.CRUD.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +18,17 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ProdutoDTO> findByID(@PathVariable Long id){
+        ProdutoDTO dto = productService.findByID(id);
+        return ResponseEntity.ok().body(dto);
+    }
 
+    @GetMapping
+    public ResponseEntity<Page<ProdutoDTO>> findAll(Pageable pageable){
+        Page<ProdutoDTO> dto = productService.findAll(pageable);
+        return ResponseEntity.ok().body(dto);
+    }
 
+    
 }
